a1a43a9446b2996bd0783ee5261f80b3c325fb77
nestaraus <nestaraus@mail.pu>
2023-10-23 21:54:34 +0300

    zadacha 5

---
+++ 5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, кторая выведет число M в степени N.
+
+int rec(int a, int b)
+{
+    if (b == 0)
+        return 1;
+    return rec(a,b-1) * a;
+}
+
+
+
+
+
+Console.Write("Введите элемент: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите элемент: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(a,b));
