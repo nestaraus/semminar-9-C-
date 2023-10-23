e88de5b367801f0a79dbbc3f0160f8ecea96785d
nestaraus <nestaraus@mail.pu>
2023-10-23 21:19:53 +0300

    zadacha 1

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int sum(int a,int b)
+{
+    if (b == 0)
+        return a;
+    return sum(a + 1, b - 1);
+}
+
+
+Console.Write("Введите элемент: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите элемент: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(a,b));
