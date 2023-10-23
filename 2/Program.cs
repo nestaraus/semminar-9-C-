207574b94e82a6b588bcf31224e7ffa4d89d598a
nestaraus <nestaraus@mail.pu>
2023-10-23 21:31:55 +0300

    zadacha 2

---
+++ 2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+string rec(int n)
+{
+    if (n == 0)
+        return "";
+    return rec(n - 1) + $"{n} ";
+}
+
+
+Console.Write("Введите элемент: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(n));
