c5f04b979efdaf2e53141eef9ed418adc97f9148
nestaraus <nestaraus@mail.pu>
2023-10-23 21:46:41 +0300

    zadacha 4

---
+++ 4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу котрая на вход будет принимать число и возвращать сумму его цифр
+
+
+int sum(int a)
+{
+    if (a<10)
+        return a;
+    return sum(a/10)+a%10;
+}
+
+
+Console.Write("Введите элемент: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(a));
